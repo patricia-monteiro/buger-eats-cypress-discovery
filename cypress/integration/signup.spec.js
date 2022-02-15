2b28221472089028b295fa721aa9e9b2093e892f
patricia-monteiro <patriciamariaakagui@gmail.com>
2022-02-15 10:34:07 -0300

    commit no git

---
+++ cypress/integration/signup.spec.js
@@ -0,0 +1,114 @@
+import signup from '../pages/SignupPage'
+import signupFactory from '../factories/SignupFactory'
+import SignupPage from '../pages/SignupPage'
+
+describe('Signup', () => {
+
+    // beforeEach(function() {
+    //     cy.fixture('deliver').then((d)=> {
+    //         this.deliver = d
+    //para pegar a massa de teste, , deliver é o nome do arquivo que ele vai puxar
+    //fixture é sincrono, portanto precisa cumprir uma promessa -> then pegar o resultado da promessa (massa de teste)
+    //then, recebe o argumento da promessa (d) abreviação de deliver
+    //this: cria uma variavel de contexto (deliver) o valor que sera armazenado é o d (que é massa teste)
+    //this para acessar a massa de teste em outros cenários de teste
+
+    //})
+    //})
+    //var signup = new SignupPage()
+    //Instanciar a classe SignupPage na variável signup,
+    //dessa forma, signup vai ter acesso a todas as funções do SignupPage
+
+    // before(function(){
+
+    //     cy.log('Tudo aqui é executado uma única vez ANTES de TODOS os casos de testes')
+    // })
+
+    // beforeEach(function(){
+    //     cy.log('Tudo aqui é executado sempre ANTES de CADA caso de teste')
+    // })
+
+    // after(function(){
+    //     cy.log('Tudo aqui é executado uma única vez DEPOIS de TODOS os casos de testes')
+    // })
+
+    // afterEach(function(){
+    //     cy.log('Tudo aqui é executado sempre DEPOIS de CADA caso de teste')
+    // })
+
+
+    it('User should be deliver', function () {
+
+        var deliver = signupFactory.deliver()
+
+        signup.go()
+        signup.fillForm(deliver)
+        signup.submit()
+
+        const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
+        signup.modalContentShouldBe(expectedMessage)
+    })
+
+    it('Incorrect document', function () {
+
+        var deliver = signupFactory.deliver()
+
+        deliver.cpf = '000000141AA'
+
+        signup.go()
+        signup.fillForm(deliver)
+        signup.submit()
+        signup.alertMessageShouldBe('Oops! CPF inválido')
+    })
+
+    it('Incorrect email', function () {
+
+        var deliver = signupFactory.deliver()
+
+        deliver.email = 'user.com.br'
+
+        signup.go()
+        signup.fillForm(deliver)
+        signup.submit()
+        signup.alertMessageShouldBe('Oops! Email com formato inválido.')
+    })
+
+    context('Required fields', function () {
+        //Caso quebrar um teste, não é bloqueado. Continua seguindo para outros testes
+
+        const messages = [
+            { field: 'name', output: 'É necessário informar o nome' },
+            { field: 'cpf', output: 'É necessário informar o CPF' },
+            { field: 'email', output: 'É necessário informar o email' },
+            { field: 'postalcode', output: 'É necessário informar o CEP' },
+            { field: 'number', output: 'É necessário informar o número do endereço' },
+            { field: 'delivery_method', output: 'Selecione o método de entrega' },
+            { field: 'cnh', output: 'Adicione uma foto da sua CNH' }
+        ]
+
+        before(function() {
+            SignupPage.go()
+            SignupPage.submit()
+        })
+
+        messages.forEach(function(msg){
+            it(`${msg.field} is required`, function(){
+                SignupPage.alertMessageShouldBe(msg.output)
+            })
+        })
+
+    })
+
+    // it('Required fields', function () {
+    //     SignupPage.go()
+    //     SignupPage.submit()
+    //     SignupPage.alertMessageShouldBe('É necessário informar o nome')
+    //     SignupPage.alertMessageShouldBe('É necessário informar o CPF')
+    //     SignupPage.alertMessageShouldBe('É necessário informar o email')
+    //     SignupPage.alertMessageShouldBe('É necessário informar o CEP')
+    //     SignupPage.alertMessageShouldBe('É necessário informar o número do endereço')
+    //     SignupPage.alertMessageShouldBe('Selecione o método de entrega')
+    //     SignupPage.alertMessageShouldBe('Adicione uma foto da sua CNH')
+
+    //})
+})
